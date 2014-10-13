4d8b17a6a392ec95cd8e44f815c86f8358d5318d
David Hanesana <dhanesana@gmail.com>
2014-10-12 23:50:39 -0700

    add solution to class2/ex3

--- lib/class2/exercise3.rb
+++ lib/class2/exercise3.rb
@@ -2,3 +2,8 @@
 #
 # If a person is 1,160 million seconds old, display their age in years. As
 # always, assume leap years don't exist.
+
+# hm soo 60 sec/min, 60 min/hour, 24 hours/day, 365 days/years
+# underscore after every 3 integers
+
+puts 1_160_000_000 / 60 / 60 / 24 / 365
