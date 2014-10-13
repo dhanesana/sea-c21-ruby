1985458be040701ecd0f4b943024341103e92112
David Hanesana <dhanesana@gmail.com>
2014-10-13 00:50:36 -0700

    add solution to class2/ex4

--- lib/class2/exercise4.rb
+++ lib/class2/exercise4.rb
@@ -5,11 +5,19 @@
 #
 # Here's how the program must work:
 #
-#   $ ruby exercise4.rb
+#   $ ruby exercise4.rb    <<< that's the command you run
 #   What's your first name?
-#   Samuel
+#   Samuel <<user types first name
 #   What's your middle name?
-#   Leroy
+#   Leroy <<user types middle name
 #   What's your last name?
-#   Jackson
+#   Jackson <<user types last name
 #   Nice to meet you, Samuel Leroy Jackson.
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your middle name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts 'Nice to meet you, ' + first_name.capitalize +
+  ' ' + middle_name.capitalize + ' ' + last_name.capitalize + '.'
