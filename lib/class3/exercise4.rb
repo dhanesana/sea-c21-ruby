83c6d4b53efea40ef7390a055a501852ad6c2117
David Hanesana <dhanesana@gmail.com>
2014-10-15 02:23:38 -0700

    add solution to class3/ex4

--- lib/class3/exercise4.rb
+++ lib/class3/exercise4.rb
@@ -8,3 +8,13 @@
 # divisible by 400 (e.g. 2000).
 #
 # Tip: I expect you to use a loop and a few modulo operations.
+
+# i suk at math tho
+# year = 1901 so 1900 isn't included
+year = 1901
+
+# year != 2001 so 2000 is included
+while year != 2001
+  puts year if year % 4 == 0 || year % 400 == 0
+  year += 1
+end
