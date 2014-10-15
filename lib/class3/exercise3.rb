5892b79935abf78e7ba3058b894629343c8e4c81
David Hanesana <dhanesana@gmail.com>
2014-10-15 01:53:04 -0700

    add solution to class3/ex3

--- lib/class3/exercise3.rb
+++ lib/class3/exercise3.rb
@@ -32,3 +32,25 @@
 #   Nana: HOW'S SCHOOL GOING?
 #   BYE
 #   Nana: BYE SWEETIE!
+
+i = 0
+puts 'Nana: HI SWEETIE! GIVE NANA A KISS!'
+
+while i != 3
+  response = gets.chomp
+  if response != response.upcase
+    puts 'Nana: HUH?! SPEAK UP, SWEETIE!'
+    i = 0
+  elsif response == response.upcase && response != 'BYE'
+    year = rand(1930..1950)
+    puts "Nana: NOT SINCE #{year}!"
+    i = 0
+  else
+    # break if i == 2 and not i == 3 to avoid +Nana: HOW'S SCHOOL GOING?
+    break if i == 2
+    puts "Nana: HOW'S SCHOOL GOING?"
+    i += 1
+  end
+end
+
+puts 'Nana: BYE SWEETIE!'
