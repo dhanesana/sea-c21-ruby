3f0f5323992d52490b247d14254bd43b8df0e8e8
David Hanesana <dhanesana@gmail.com>
2014-10-20 09:46:22 -0700

    modify class4/ex2:34

--- lib/class4/exercise2.rb
+++ lib/class4/exercise2.rb
@@ -17,7 +17,8 @@
 # TIP #2: Use the `string.ljust` method from class 2 exercise 7.
 #
 # TIP #3: The width of each line is 50 characters.
-
+=begin
+# multiple arrays
 title = ['Can I Kick It?', 'Rump Shaker', 'Check Yo Self', 'Regulate', \
          'I Got 5 On It', 'Ready Or Not']
 artist = ['A Tribe Called Quest', 'Wreckx-n-Effect', 'Ice Cube', \
@@ -27,4 +28,18 @@ year = [1991, 1992, 1993, 1994, 1995, 1996]
 (0..5).each do |x|
   puts "#{title[x]}".ljust(20) + "#{artist[x]}".ljust(26) + \
        "#{year[x]}".ljust(4)
+end
+=end
+
+# single array
+array = [['Can I Kick It?', 'A Tribe Called Quest', '1991'],
+         ['Rump Shaker', 'Wreckx-n-Effect', '1992'],
+         ['Check Yo Self', 'Ice Cube', '1993'],
+         ['Regulate', 'Warren G & Nate Dogg', '1994'],
+         ['I Got 5 On It', 'Luniz', '1995'],
+         ['Ready Or Not', 'The Fugees', '1996']
+        ]
+
+array.each do |x|
+  puts x[0].ljust(20) + x[1].ljust(26) + x[2].ljust(4)
 end
