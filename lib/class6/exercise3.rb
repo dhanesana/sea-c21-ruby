3565f2e3992cf1d93e077d13dcc96eb30ae99640
David Hanesana <dhanesana@gmail.com>
2014-10-22 21:05:08 -0700

    add solution to class6/ex3

--- lib/class6/exercise3.rb
+++ lib/class6/exercise3.rb
@@ -27,15 +27,22 @@
 require 'yaml'
 
 def person
-  { replace: 'me' }
+  # { key: value }
+  {
+    name: 'George Harrison',
+    age: 58,
+    song: 'Something',
+    url: 'https://www.youtube.com/watch?v=UKAp-jRUp2o'
+  }
 end
 
 def database
-  '/replace/me'
+  # File.dirname(File.absolute_path(__FILE__)) + '/database.yml'
+  File.absolute_path('../database.yml', __FILE__)
 end
 
 def save
-  false # fix me
+  File.write(database, person.to_yaml)
 end
 
 save
